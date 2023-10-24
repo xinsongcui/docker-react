5a843fb51ad2c186e42b25afe8212bff064e8ea3
Xinsong Cui <xinsong@Xinsongs-MacBook-Pro.local>
2023-10-23 23:56:30 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hi, I'm <code>src/App.js</code> and I was changed on the feature branch.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
